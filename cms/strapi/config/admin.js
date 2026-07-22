6b39e0f9990b94452407fe878d163157136b8aa3
Vishwapathi Aravindh <vishwapathiaravindh@gmail.com>
2026-07-22 11:11:45 +0530

    fix: add fallback keys and secrets to server/admin config to prevent Render startup crash

--- cms/strapi/config/admin.js
+++ cms/strapi/config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'fallbackAdminJwtSecretValueChangeMeInProduction123!'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'fallbackApiTokenSaltValueChangeMeInProduction123!'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'fallbackTransferTokenSaltValueChangeMe123!'),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', 'fallbackEncryptionKeyChangeMe123!'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
--- cms/strapi/config/server.js
+++ cms/strapi/config/server.js
@@ -1,10 +1,13 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  const fallbackKeys = ['fallbackAppKeyA123!', 'fallbackAppKeyB123!', 'fallbackAppKeyC123!', 'fallbackAppKeyD123!'];
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS', fallbackKeys),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
