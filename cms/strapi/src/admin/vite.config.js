7ec73acd1112e2fb3e42090277a63810498694a1
Vishwapathi Aravindh <vishwapathiaravindh@gmail.com>
2026-07-22 11:51:47 +0530

    fix: add admin vite.config.js to alias react packages and prevent duplicate context bundling

---
+++ cms/strapi/src/admin/vite.config.js
@@ -0,0 +1,15 @@
+const { mergeConfig } = require('vite');
+const path = require('path');
+
+module.exports = (config) => {
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        'react': path.resolve(__dirname, '../../node_modules/react'),
+        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
+        'react-router-dom': path.resolve(__dirname, '../../node_modules/react-router-dom'),
+        'styled-components': path.resolve(__dirname, '../../node_modules/styled-components'),
+      },
+    },
+  });
+};
